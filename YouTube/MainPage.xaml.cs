f908a599ac37abec7910bda9a9ce123d280764db
Shea Smith <51303984+SheaSmith@users.noreply.github.com>
2021-12-30 09:55:25 +1300

    Init

---
+++ YouTube/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace YouTube
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public async void PageLoaded(object sender, RoutedEventArgs e)
+        {
+            await wv2.EnsureCoreWebView2Async();
+            wv2.CoreWebView2.Settings.UserAgent = "Xbox/2.0.4548.0 UPnP/1.0 Xbox/2.0.4548.0";
+        }
+    }
+}
